741e84c07d759845a63b9b7310d0413b2db5012a
kana-328 <kana-328@i.softbank.jp>
2020-11-22 23:31:51 +0900

    carrywaveの本番環境設定

--- app/models/package.rb
+++ app/models/package.rb
@@ -5,4 +5,14 @@ class Package < ApplicationRecord
   has_one :city
   has_one :prefecture
   mount_uploader :image, ImageUploader
+  validate  :image_size
+
+  private
+
+    # アップロードされた画像のサイズをバリデーションする
+    def image_size
+      if image.size > 5.megabytes
+        errors.add(:image, "should be less than 5MB")
+      end
+    end
 end
