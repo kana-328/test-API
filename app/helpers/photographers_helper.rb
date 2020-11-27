39f8afeefaee0e3984a62579664899fb69e5708c
kana-328 <kana-328@i.softbank.jp>
2020-11-27 22:03:42 +0900

    package/show編集

--- app/helpers/photographers_helper.rb
+++ app/helpers/photographers_helper.rb
@@ -2,7 +2,7 @@ module PhotographersHelper
   def gravatar_for(photographer, options = { size: 80})
     gravatar_id = Digest::MD5::hexdigest(photographer.email.downcase)
     size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?d=robohash&?s=#{size}"
     image_tag(gravatar_url,alt: photographer.email, class:"gravatar")
    end
 end
